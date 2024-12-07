dc6f83b203cc62f5ca9839d7cff90ec05fb54c76
Arnav Kirti <arnav.tkd@gmail.com>
2024-12-07 22:28:10 +0530

    added some logic to solve puzzle

--- puzzle.c
+++ puzzle.c
@@ -1,4 +1,36 @@
-#include "sudoku.h" 
+#include "sudoku.h"
+
+Square ***setUpPuzzle(int **puzzle)
+{
+    Square ***sudoku;
+    int i, j;
+
+    sudoku = (Square ***)malloc(sizeof(Square **) * 9);
+
+    for (i = 0; i < 9; i++)
+    {
+        sudoku[i] = (Square **)malloc(sizeof(Square *) * 9);
+        puzzle[i] = (int *)malloc(sizeof(int *) * 9);
+
+        for (j = 0; j < 9; j++)
+        {
+            sudoku[i][j] = (Square *)malloc(sizeof(Square) * 9);
+            sudoku[i][j]->number = puzzle[i][j];
+
+            sudoku[i][j]->row = i;
+            sudoku[i][j]->column = j;
+
+            if (sudoku[i][j]->number != 0)
+            {
+                sudoku[i][j]->code = POSSIBLE;
+            }
+            else
+            {
+                sudoku[i][j]->code = 0x0;
+            }
+        }
+    }
+}
 
 int **createPuzzle()
 {
@@ -38,18 +70,20 @@ void printPuzzle(int **puzzle)
     printf("-------------------------------\n");
     for (i = 0; i < 9; i++)
     {
-        printf("|"); 
+        printf("|");
         for (j = 0; j < 9; j++)
         {
             printf(" %d ", puzzle[i][j]);
 
-            if((j+1)%3 == 0){
+            if ((j + 1) % 3 == 0)
+            {
                 printf("|");
             }
         }
         printf("\n");
-        if((i+1)%3==0){
-            printf("-------------------------------\n"); 
+        if ((i + 1) % 3 == 0)
+        {
+            printf("-------------------------------\n");
         }
     }
 }
